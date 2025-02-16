8214af296ab1dd550b4020c565431ac1c166720c
ved1nsh <vedansh.sharma.vs17@gmail.com>
2025-02-17 00:34:49 +0530

    Initial Commit

---
+++ Barcode Scanner App/Barcode_Scanner_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_Scanner_AppApp.swift
+//  Barcode Scanner App
+//
+//  Created by Vedansh Sharma on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_Scanner_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barcode Scanner AppTests/Barcode_Scanner_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Barcode_Scanner_AppTests.swift
+//  Barcode Scanner AppTests
+//
+//  Created by Vedansh Sharma on 2/17/25.
+//
+
+import Testing
+
+struct Barcode_Scanner_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
